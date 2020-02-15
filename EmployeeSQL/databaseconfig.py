325a699144fd218774e2ff6e7b937181c3019718
Patrick Patin <40468092+patrickhpatin@users.noreply.github.com>
2020-02-15 10:40:36 -0500

    Update databaseconfig.py

--- EmployeeSQL/databaseconfig.py
+++ EmployeeSQL/databaseconfig.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python
-import preprocessing
 mysql = {'host': 'localhost',
          'user': '<your user>',
          'passwd': '<your password>',
          'db': '<your db name'}
-preprocessing_queue = [preprocessing.scale_and_center,
-                       preprocessing.dot_reduction,
-                       preprocessing.connect_lines]
-use_anonymous = True
